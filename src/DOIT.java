804681faf47e7c17d05ef009bfa429030ac5d579
Triplefonk <like42day@gmail.com>
2023-08-20 21:09:26 +0300

    Commit it bro

--- src/DOIT.java
+++ src/DOIT.java
@@ -1,5 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class DOIT {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader triplefonk = new BufferedReader(new InputStreamReader(System.in));
+
+        Integer [] io = new Integer[5];
+        for (int i = 0; i < io.length; i++) {
+
+            io[i] = Integer.parseInt(triplefonk.readLine());
+
+
+        }
+         /* comfortable way to reverse numbers */
+        Arrays.sort(io, Collections.reverseOrder());
+        System.out.println(Arrays.toString(io));
+
+        /* Tough way to reverse numbers...
+
+        if (io[0] < io[1] & io[0] < io[2] & io[0] < io[3] & io[0] < io[4]){
+            if (io[1] < io[2] & io[1] < io[3] & io[1] < io[4]) {
+                if (io[2] < io[3] & io[2] < io[4]){
+                    if (io[3] < io[4]){
+
+                        System.out.println(io[4]+" "+io[3]+" "+io[2]+" "+io[1]+" "+io[0]);
+                        }
+                    }
+                }
+
+            }
+
+        if (io[1] < io[0] & io[1] < io[2] & io[1] < io[3] & io[1] < io[4]){
+            if (io[0] < io[2] & io[0] < io[3] & io [0] < io[4]) {
+                if (io [2] < io [3] & io[2] < io[4]){
+                    if (io [3] < io [4]){
+                        System.out.println(io[4] + " "+ io [3]+ " "+ io [2]+ " "+ io[0]+ " "+io [1]);
+                    }
+                }
+            }
+        }
+        */
+
 
     }
 }
