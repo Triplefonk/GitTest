b4103f999d79aca9aa9b140f923ec3d732534a72
Triplefonk <like42day@gmail.com>
2023-08-20 21:10:35 +0300

    Commit it bro

--- src/DOIT.java
+++ src/DOIT.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
+
 
 public class DOIT {
     public static void main(String[] args) throws IOException {
